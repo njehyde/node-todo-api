c0d8f63cceb7d1259bd29af0e0db9776b4a95d87
Nicholas Hyde <njehyde@gmail.com>
2018-03-12 07:21:37 +0000

    Add delete script

---
+++ playground/mongodb-delete.js
@@ -0,0 +1,34 @@
+const {MongoClient, ObjectID} = require('mongodb');
+
+MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, client) => {
+    if (err) {
+        return console.log('Unable to connect to MongoDB server');
+    }
+    console.log('Connected to MongoDB server');
+    const db = client.db('TodoApp');
+
+    // deleteMany
+    // db.collection('Todos').deleteMany({text: 'Eat lunch'}).then((result) => {
+    //     console.log(result);
+    // });
+
+    // deleteOne
+    // db.collection('Todos').deleteOne({text:'Eat lunch'}).then((result) => {
+    //     console.log(result);
+    // });
+
+    // findOneAndDelete
+    // db.collection('Todos').findOneAndDelete({completed: false}).then((result) => {
+    //     console.log(result);
+    // });
+
+    // db.collection('Users').deleteMany({name: 'Nicholas Hyde'});
+
+    db.collection('Users').findOneAndDelete({
+        _id: new ObjectID("5aa629592f5eea42aa70d4b0")
+    }).then((result) => {
+        console.log(JSON.stringify(result));
+    });
+
+    client.close();
+});
--- playground/mongodb-find.js
+++ playground/mongodb-find.js
@@ -7,18 +7,18 @@ MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, client) => {
     console.log('Connected to MongoDB server');
     const db = client.db('TodoApp');
 
-    // db.collection('Todos').find({
-    //     _id: new ObjectID('5aa4ec76c3ba19056b969a57')
-    // }).toArray().then((docs) => {
-    //     console.log('Todos');
-    //     console.log(JSON.stringify(docs, undefined, 2));
-    // }, (err) => {
-    //     console.log('Unable to fetch todos', err);
-    // });
+    db.collection('Todos').find({
+        //_id: new ObjectID('5aa4ec76c3ba19056b969a57')
+        text: 'Eat Lunch'
+    }).toArray().then((docs) => {
+        console.log('Todos');
+        console.log(JSON.stringify(docs, undefined, 2));
+    }, (err) => {
+        console.log('Unable to fetch todos', err);
+    });
 
     db.collection('Todos').find().count().then((count) => {
         console.log(`Todos count: ${count}`);
-        
     }, (err) => {
         console.log('Unable to fetch todos', err);
     });
