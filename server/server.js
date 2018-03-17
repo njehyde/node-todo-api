b42b24c9d7ebf838457e68f1e73c8bbf400b0843
Nicholas Hyde <njehyde@gmail.com>
2018-03-17 17:11:38 +0000

    Add DELETE /users/me/token route

--- server/server.js
+++ server/server.js
@@ -106,7 +106,7 @@ app.post('/users', (req, res) => {
     var body = _.pick(req.body, ['email', 'password']);
     var user = new User(body);
 
-    user.save(body).then(() => {
+    user.save().then(() => {
         return user.generateAuthToken();
     }).then((token) => {
         res.header('x-auth', token).send(user);
@@ -123,12 +123,20 @@ app.post('/users/login', (req, res) => {
    var body = _.pick(req.body, ['email', 'password']);
    
    User.findByCredentials(body.email, body.password).then((user) => {
-        user.generateAuthToken().then((token) => {
+        return user.generateAuthToken().then((token) => {
             res.header('x-auth', token).send(user);
         });
    }).catch((e) => {
        res.status(400).send();
    });
+});
+
+app.delete('/users/me/token', authenticate, (req, res) => {
+    req.user.removeToken(req.token).then(() => {
+        res.status(200).send();
+    }, () => {
+        res.status(400).send();
+    });
 });
 
 app.listen(port, () => {
